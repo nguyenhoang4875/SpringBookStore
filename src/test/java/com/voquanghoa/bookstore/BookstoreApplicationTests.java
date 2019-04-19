d00a17afa07ecf5c53b268f1e146cc5924ed7185
Võ Quang Hòa <hvquang@openwt.com>
2019-04-19 16:40:33 +0700

    Update

--- src/test/java/com/voquanghoa/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/voquanghoa/bookstore/BookstoreApplicationTests.java
@@ -3,6 +3,7 @@ package com.voquanghoa.bookstore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class BookstoreApplicationTests {
 
 	@Value("${spring.datasource.url}")
--- src/test/java/com/voquanghoa/bookstore/UserRepositoryTest.java
+++ src/test/java/com/voquanghoa/bookstore/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import com.voquanghoa.bookstore.repositories.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,6 +14,7 @@ import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class UserRepositoryTest {
 
     @Autowired
