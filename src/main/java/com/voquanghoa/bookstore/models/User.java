1cc349d8f2c7b0164b7245784a66c3749e225c30
Võ Quang Hòa <hvquang@openwt.com>
2019-04-19 16:12:00 +0700

    Update

--- src/main/java/com/voquanghoa/bookstore/models/User.java
+++ src/main/java/com/voquanghoa/bookstore/models/User.java
@@ -39,7 +39,7 @@ public class User {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-            name = "users_role",
+            name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
