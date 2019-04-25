5d0702372a177bafe8020a229be74e1236d3ba60
Võ Quang Hòa <hvquang@openwt.com>
2019-04-25 16:23:12 +0700

    Update

--- src/test/java/com/voquanghoa/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/voquanghoa/bookstore/BookstoreApplicationTests.java
@@ -2,27 +2,33 @@ package com.voquanghoa.bookstore;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class BookstoreApplicationTests {
 
-	@Value("${spring.datasource.url}")
-	private String dataSourceUrl;
+	@Value("${profile.testing}")
+	private boolean isTest;
+
+	@Autowired
+	private Environment environment;
 
 	@Test
 	public void contextLoads() {
+		System.out.println("Profile = " + String.join(",",  environment.getActiveProfiles()));
 	}
 
 	@Test
-	public void test_dataSourceUrl(){
-		assertEquals(dataSourceUrl, "jdbc:postgresql://localhost:5432/circle_test");
+	public void test_isTesting(){
+		assertTrue(isTest);
 	}
 }
